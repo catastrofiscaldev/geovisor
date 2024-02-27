9ae32c436da4f9e952d824732070b45c699a1c6e
danielgis <daniel030891@gmail.com>
2024-02-27 13:04:52 -0500

    Se agrego la libreria open layers y el mapa al geovisor

--- src/app/modules/landing/geovisor/map/map.component.ts
+++ src/app/modules/landing/geovisor/map/map.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
+import { OverviewMap, defaults as defaultControls, Zoom } from 'ol/control';
 
 @Component({
     selector: 'map',
@@ -11,4 +17,41 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
     styleUrl: './map.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MapComponent { }
+export class MapComponent implements OnInit {
+    @ViewChild('mapElement', { static: true }) mapElement: ElementRef;
+
+    ngOnInit(): void {
+        this.initializeMap();
+    }
+
+    private initializeMap(): void {
+        const map = new Map({
+            target: this.mapElement.nativeElement,
+            layers: [
+                new TileLayer({
+                    source: new OSM(),
+                }),
+            ],
+            view: new View({
+                center: fromLonLat([-75.0152, -9.189967]),
+                zoom: 6,
+            }),
+            controls: [],
+        });
+        const zoomControl = new Zoom({
+            className: 'custom-zoom',
+            target: document.getElementById('map-zoom-control')
+        });
+        map.addControl(zoomControl);
+
+        // const overviewMapControl = new OverviewMap({
+        //     layers: [
+        //         new TileLayer({
+        //             source: new OSM(),
+        //         }),
+        //     ],
+        // });
+
+        // map.addControl(overviewMapControl);
+    }
+}
